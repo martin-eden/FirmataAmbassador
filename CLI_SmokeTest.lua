ccc80c3104fcfabcf8e70298c1dcb739b097a3af
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2023-06-13 18:24:35 -0400

    [^] [CLI_SmokeTest] Better code structure.

--- CLI_SmokeTest.lua
+++ CLI_SmokeTest.lua
@@ -1,7 +1,8 @@
 #!/bin/lua
 
 --[[
-  Command-line tool to test general functionality of my Firmata library.
+  Command-line tool to test general functionality of my FirmataAmbassador
+  library.
 
   Usage:
 
@@ -10,12 +11,32 @@
 
 --[[
   Status: works
-  Version: 4
-  Last mod.: 2023-06-08
+  Version: 5
+  Last mod.: 2023-06-13
 ]]
 
--- local PortName = '/dev/ttyUSB0' -- Arduino Uno
-local PortName = '/dev/ttyACM0' -- Arduino Mega
+-- Configuration:
+
+local PortName = '/dev/ttyUSB0' -- Arduino Uno
+-- local PortName = '/dev/ttyACM0' -- Arduino Mega
+
+local TestsToRun =
+  {
+    -- DiscoverUsbPorts = true,
+    -- ScanI2c = true,
+
+    -- GetVersion = true,
+    -- GetVersionAndName = true,
+
+    GetBoardConfiguration = true,
+
+    DigitalRead = true,
+    DigitalWrite = true,
+    AnalogRead = true,
+    AnalogWrite = true,
+  }
+
+-- Implementation:
 
 package.path = package.path .. ';../../../?.lua'
 require('workshop.base')
@@ -33,8 +54,7 @@ local Represent =
     print(GetResponseRepresentation(Message))
   end
 
---[[
-do
+if TestsToRun.DiscoverUsbPorts then
   local FirmataPorts = Firmata:GetFirmataUsbPorts()
   Represent('USB ports with Firmata', FirmataPorts)
 
@@ -43,97 +63,84 @@ do
     return
   end
 end
---]]
 
-do
-  print(('Connecting to port "%s".'):format(PortName))
-  Firmata:ConnectTo(PortName)
-end
-
--- [[
-do
-  print('Scanning for I2C devices.')
-  local I2cDevices = Firmata:GetOpenedI2cPorts()
-  Represent('I2C devices', I2cDevices)
-end
---]]
-
--- [[
-do
-  local Version = Firmata:GetVersion()
-  Represent('Version', Version)
-end
---]]
-
--- [[
-do
-  local VersionAndFirmware = Firmata:GetVersionAndName()
-  Represent('Version and firmware', VersionAndFirmware)
-end
---]]
-
--- [[
-do
-  print('Digital reads.')
-  for pin = 0, 13 do
-    print(('Pin %d: %s.'):format(pin, Firmata:DigitalRead({Pin = pin})))
-  end
-end
---]]
-
--- [[
-do
-  print('Digital writes.')
-  for i = 1, 5 do
-    Firmata:DigitalWrite({Pin = 13, Value = true})
-    print('Tic.')
-    SleepSec(0.5)
-    Firmata:DigitalWrite({Pin = 13, Value = false})
-    print('Tac.')
-    SleepSec(0.5)
-  end
-end
---]]
-
--- [[
-do
-  print('Analog reads.')
-  for pin = 0, 5 do
-    print(('A%d: %.2f'):format(pin, Firmata:AnalogRead({Pin = pin}) or -1))
-  end
-end
---]]
-
--- [[
-do
-  print('Analog (PWM) writes.')
-
-  local Pin = 3
-
-  print(('Using pin %d for PWM output.'):format(Pin))
-
-  local N = 16
-  for i = 1, N do
-    local Value = (i / N) - 1 / 512
-    print(('[%d / %d]: %.2f'):format(i, N, Value))
-    Firmata:AnalogWrite({Pin = Pin, Value = Value})
-    SleepSec(.1)
-  end
-  SleepSec(1.5)
-  Firmata:AnalogWrite({Pin = Pin, Value = 0.0})
-end
---]]
-
--- [[
-  do
-    print('Getting board configuration.')
-    local BoardConfiguration = Firmata:GetBoardConfiguration()
-    if BoardConfiguration then
-      -- Represent('', BoardConfiguration)
-      print(t2s(BoardConfiguration))
+local RunTests =
+  function(TestsToRun)
+    if TestsToRun.ScanI2c then
+      print('Scanning for I2C devices.')
+      local I2cDevices = Firmata:GetOpenedI2cPorts()
+      Represent('I2C devices', I2cDevices)
     end
-  end
---]]
+
+    if TestsToRun.GetVersion then
+      local Version = Firmata:GetVersion()
+      Represent('Version', Version)
+    end
+
+    if TestsToRun.GetVersionAndName then
+      local VersionAndFirmware = Firmata:GetVersionAndName()
+      Represent('Version and firmware', VersionAndFirmware)
+    end
+
+    if TestsToRun.GetBoardConfiguration then
+      print('Getting board configuration.')
+      local BoardConfiguration = Firmata:GetBoardConfiguration()
+      if BoardConfiguration then
+        -- Represent('', BoardConfiguration)
+        print(t2s(BoardConfiguration))
+      end
+    end
+
+    if TestsToRun.DigitalRead then
+      print('Digital reads.')
+      for pin = 0, 13 do
+        print(('Pin %d: %s.'):format(pin, Firmata:DigitalRead(pin)))
+      end
+    end
+
+    if TestsToRun.DigitalWrite then
+      print('Digital writes.')
+      for i = 1, 5 do
+        Firmata:DigitalWrite({Pin = 13, Value = true})
+        print('Tic.')
+        SleepSec(0.5)
+        Firmata:DigitalWrite({Pin = 13, Value = false})
+        print('Tac.')
+        SleepSec(0.5)
+      end
+      Firmata:DigitalWrite({Pin = 10, Value = true})
+    end
+
+    if TestsToRun.AnalogRead then
+      print('Analog reads.')
+      for pin = 0, 5 do
+        print(('A%d: %.2f'):format(pin, Firmata:AnalogRead(pin) or -1))
+      end
+    end
+
+    if TestsToRun.AnalogWrite then
+      print('Analog (PWM) writes.')
+
+      local Pin = 3
+
+      print(('Using pin %d for PWM output.'):format(Pin))
+
+      local N = 16
+      for i = 1, N do
+        local Value = (i / N) - 1 / 512
+        print(('[%d / %d]: %.2f'):format(i, N, Value))
+        Firmata:AnalogWrite({Pin = Pin, Value = Value})
+        SleepSec(.1)
+      end
+      SleepSec(1.5)
+      Firmata:AnalogWrite({Pin = Pin, Value = 0.0})
+    end
+end
+
+print(('Connecting to port "%s".'):format(PortName))
+Firmata:ConnectTo(PortName)
+
+RunTests(TestsToRun)
 
 print('Disconnected.')
 Firmata:Disconnect()
