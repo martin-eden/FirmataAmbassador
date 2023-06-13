f428933c6b61690475f4ff4a952469f57feace64
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2023-06-13 19:19:14 -0400

    [^] [Interface] Finally separated interface between public and internal.

--- FirmataAmbassador/Interface.lua
+++ FirmataAmbassador/Interface.lua
@@ -1,9 +1,15 @@
--- Error output. Feel free to redefine.
--- For sure definition of global function will be removed from here.
+--[[
+  FirmataAmbassador library public interface.
+]]
 
+-- For sure definition of global function will be removed from here.
+-- Error output function. Feel free to redefine.
 _G.Complain = function(Msg) print(Msg) end
 
-return
+local Internals = request('Internals.Interface')
+local MergeTable = request('!.table.merge')
+
+local Result =
   {
     -- Connection management:
     ConnectTo = request('ConnectTo'),
@@ -12,31 +18,14 @@ return
     GetPortName = function(self) return self.Connector.PortName end,
 
     -- Main functions:
-    I2cRead = request('I2cRead'),
-    I2cWrite = request('I2cWrite'),
     DigitalRead = request('DigitalRead'),
     DigitalWrite = request('DigitalWrite'),
     AnalogRead = request('AnalogRead'),
     AnalogWrite = request('AnalogWrite'),
-
-    -- Implementation internals:
-    Connector = request('Internals.Connector.Interface'),
-    Transmitter = request('Internals.Transmitter.Interface'),
-    Parser = request('Internals.Parser.Interface'),
-
-    SetPinMode = request('Internals.SetPinMode'),
-    SetPinValue = request('Internals.SetPinValue'),
-    Send = request('Internals.Send'),
-    CompileAndSend = request('Internals.CompileAndSend'),
-    Receive = request('Internals.Receive'),
-    CompileSendAndReceive = request('Internals.CompileSendAndReceive'),
-    GotInitGreetings = request('Internals.GotInitGreetings'),
-
-    Reset = request('Internals.Reset'),
-    GetBoardConfiguration = request('Internals.GetBoardConfiguration'),
-    GetVersion = request('Internals.GetVersion'),
-    GetVersionAndName = request('Internals.GetVersionAndName'),
-
-    I2cInit = request('Internals.I2cInit'),
-    IsI2cInitialized = false,
+    I2cRead = request('I2cRead'),
+    I2cWrite = request('I2cWrite'),
   }
+
+MergeTable(Result, Internals)
+
+return Result
---
+++ FirmataAmbassador/Internals/Interface.lua
@@ -0,0 +1,28 @@
+--[[
+  Implementation internals for FirmataAmbassador library.
+
+  As with any internals they are subject to frequent breaking changes.
+]]
+
+return
+  {
+    Connector = request('Connector.Interface'),
+    Transmitter = request('Transmitter.Interface'),
+    Parser = request('Parser.Interface'),
+
+    SetPinMode = request('SetPinMode'),
+    SetPinValue = request('SetPinValue'),
+    Send = request('Send'),
+    CompileAndSend = request('CompileAndSend'),
+    Receive = request('Receive'),
+    CompileSendAndReceive = request('CompileSendAndReceive'),
+    GotInitGreetings = request('GotInitGreetings'),
+
+    Reset = request('Reset'),
+    GetBoardConfiguration = request('GetBoardConfiguration'),
+    GetVersion = request('GetVersion'),
+    GetVersionAndName = request('GetVersionAndName'),
+
+    I2cInit = request('I2cInit'),
+    IsI2cInitialized = false,
+  }
